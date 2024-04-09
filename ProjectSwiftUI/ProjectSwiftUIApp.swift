9f36092a77337a3dec28147aa392f43a06c2c9d0
VladislavDani <vl9719@mail.ru>
2024-04-09 11:22:58 +0300

    Initial Commit

---
+++ ProjectSwiftUI/ProjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSwiftUIApp.swift
+//  ProjectSwiftUI
+//
+//  Created by Владислав Даниленко on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
